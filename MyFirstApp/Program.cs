9bcc0c9b419e00946a02cb54c13f09f4ea4deb7c
rasmu <rasmu@MSI>
2023-02-09 21:07:04 +0400

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Media;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "aaa";
+            string b = "aaa";
+            
+            Console.WriteLine();
+            // int number1 = 8;
+            // int number = 80085;
+            Console.WriteLine();
+           
+           // short num2 = 8;
+           // byte num3 = 8;
+           // long num4 = 8;
+           // uint num5 = 8;
+           // long num6 = 8;
+           // short num7 = 8; 
+           // float num8 = 8.78899f;
+
+           // int num = 21;
+           // int num9 = 8;
+           // int n = num * number1;
+
+           // int sum1 = number1 + number;
+           // int division = number1 / number;
+           // int div = number1 % division;
+           // Console.WriteLine(number1);
+           // Console.WriteLine(num2);
+           // Console.WriteLine(num3);
+           // Console.WriteLine(num4);
+           // Console.WriteLine(num5);
+           // Console.WriteLine(num6);
+           // Console.WriteLine(num7);
+           // Console.WriteLine(num8);
+           // Console.WriteLine(n);
+           // Console.WriteLine(division);
+           // Console.WriteLine(div);
+           
+
+
+           // //System.Console.Beep(1000,100);
+           // Console.ReadKey();
+
+           //// Console.WriteLine("Hello, World!");
+        }
+    }
+}
